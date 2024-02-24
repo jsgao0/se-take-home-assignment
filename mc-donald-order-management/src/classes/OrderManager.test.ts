c359b462a225d77c4672f629f66202412b50d6c6
Anson Gao <s1496k030@gmail.com>
2024-02-24 22:04:31 +0800

    add unit tests for `OrderManager`

---
+++ mc-donald-order-management/src/classes/OrderManager.test.ts
@@ -0,0 +1,90 @@
+import { OrderStatus, OrderType } from "../types";
+import OrderManager from "./OrderManager";
+
+describe('OrderManager', () => {
+  it('init OrderManager', () => {
+    const orderManager = new OrderManager();
+    expect(orderManager.orders).toEqual([]);
+  });
+
+  it('addOrder', () => {
+    const orderManager = new OrderManager();
+    orderManager.addOrder({
+      type: OrderType.Normal,
+      status: OrderStatus.Pending,
+    });
+    expect(orderManager.orders).toEqual([
+      {
+        id: 1, // the first order
+        type: OrderType.Normal,
+        status: OrderStatus.Pending,
+      }
+    ]);
+
+    orderManager.addOrder({
+      type: OrderType.Normal,
+      status: OrderStatus.Pending,
+    });
+    expect(orderManager.orders).toEqual([
+      {
+        id: 1, // the first order
+        type: OrderType.Normal,
+        status: OrderStatus.Pending,
+      },
+      {
+        id: 2, // the second order
+        type: OrderType.Normal,
+        status: OrderStatus.Pending,
+      }
+    ]);
+  });
+
+  it('getOrders', () => {
+    const orderManager = new OrderManager();
+    orderManager.addOrder({
+      type: OrderType.Normal,
+      status: OrderStatus.Pending,
+    });
+    orderManager.addOrder({
+      type: OrderType.Normal,
+      status: OrderStatus.Processing,
+    });
+    orderManager.addOrder({
+      type: OrderType.Normal,
+      status: OrderStatus.Complete,
+    });
+    expect(orderManager.getOrders([OrderStatus.Pending])).toEqual([
+      {
+        id: 1,
+        type: OrderType.Normal,
+        status: OrderStatus.Pending,
+      }
+    ]);
+    expect(orderManager.getOrders([OrderStatus.Processing])).toEqual([
+      {
+        id: 2,
+        type: OrderType.Normal,
+        status: OrderStatus.Processing,
+      }
+    ]);
+    expect(orderManager.getOrders([OrderStatus.Complete])).toEqual([
+      {
+        id: 3,
+        type: OrderType.Normal,
+        status: OrderStatus.Complete,
+      }
+    ]);
+    expect(orderManager.getOrders([OrderStatus.Pending, OrderStatus.Complete])).toEqual([
+      {
+        id: 1,
+        type: OrderType.Normal,
+        status: OrderStatus.Pending,
+      },
+      {
+        id: 3,
+        type: OrderType.Normal,
+        status: OrderStatus.Complete,
+      }
+    ]);
+  });
+});
